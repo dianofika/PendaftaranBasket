c9d8d4628bf98504f0cc11a427b027a6dae21969
Dian <diannofika.dn@gmail.com>
2016-10-16 07:43:35 +0700

    #commitfinal - sudah jadi

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3008/pendaftaranbasket/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3008/pendaftaranbasket/MainActivity.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,7 +18,13 @@ public class MainActivity extends AppCompatActivity {
     EditText etNomor;
     EditText etAlamat;
     TextView tvHasil;
+    TextView etJK;
     Button bOK;
+    RadioButton rb1;
+    RadioButton rb2;
+    RadioGroup rgStatus;
+    Spinner sp;
+    CheckBox popda, o2sn, kobalajar, tdk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +37,15 @@ public class MainActivity extends AppCompatActivity {
         etAlamat = (EditText) findViewById(R.id.editTextAlamat);
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
         bOK = (Button) findViewById(R.id.button);
+        rb1 = (RadioButton) findViewById(R.id.radioButtonPerempuan);
+        rb2 = (RadioButton) findViewById(R.id.radioButtonLaki);
+        etJK = (TextView) findViewById(R.id.textView6);
+        rgStatus = (RadioGroup) findViewById(R.id.RadioGroupStatus);
+        sp = (Spinner) findViewById(R.id.spinner);
+        popda = (CheckBox) findViewById(R.id.checkBoxPopda);
+        o2sn = (CheckBox) findViewById(R.id.checkBoxO2sn);
+        kobalajar = (CheckBox) findViewById(R.id.checkBoxKobalajar);
+        tdk = (CheckBox) findViewById(R.id.checkBox);
 
         bOK.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,46 +57,54 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void doProses() {
-        if (isValid()) {
+
             String nama = etNama.getText().toString();
             String sekolah = etSekolah.getText().toString();
             int nomor = Integer.parseInt(etNomor.getText().toString());
             String alamat = etAlamat.getText().toString();
-            tvHasil.setText("\n Nama        : " + nama + "\n Sekolah    : " + sekolah + "\n No.Telp     : " + nomor + "\n Alamat      : " + alamat);
+        String hasil = "\n Jenis Kelamin    : ";
+        String posisi = sp.getSelectedItem().toString();
+        String pres = "\n Prestasi      : ";
+
+        int startlen = pres.length();
+        if (popda.isChecked()) pres += popda.getText() + ",";
+        if (o2sn.isChecked()) pres += o2sn.getText() + ",";
+        if (kobalajar.isChecked()) pres += kobalajar.getText() + ",";
+        if (tdk.isChecked()) pres += tdk.getText();
+        if (pres.length() == startlen) pres += "Anda belum memilih";
+
+        if (rgStatus.getCheckedRadioButtonId() != -1) {
+            RadioButton rb = (RadioButton)
+                    findViewById(rgStatus.getCheckedRadioButtonId());
+            hasil = rb.getText().toString();
         }
-    }
-
-    private boolean isValid() {
-
-        boolean valid = true;
-
-        String nama = etNama.getText().toString();
-        String sekolah = etSekolah.getText().toString();
-        String alamat = etAlamat.getText().toString();
 
         if (nama.isEmpty()) {
             etNama.setError("Nama Belum Diisi");
-            valid = false;
+
         } else if (nama.length() < 3) {
             etNama.setError("Nama Minimal 3 Karakter");
-            valid = false;
+
         } else {
             etNama.setError(null);
         }
 
+
         if (sekolah.isEmpty()) {
             etSekolah.setError("Sekolah Belum Diisi");
-            valid = false;
+
         } else {
             etSekolah.setError(null);
         }
 
         if (alamat.isEmpty()) {
             etAlamat.setError("Alamat Belum Diisi");
-            valid = false;
+
         } else {
             etAlamat.setError(null);
         }
-        return valid;
+
+        tvHasil.setText("-----DATA TERKIRIM-----" + "\nNama       : " + nama + "\n Sekolah     : " + sekolah
+                + "\n No.Telp     : " + nomor + "\nAlamat     : " + alamat + "\nJenis Kelamin : " + hasil + "\n Prestasi   : " + pres + "\n Posisi    : " + sp.getSelectedItem().toString());
     }
 }
